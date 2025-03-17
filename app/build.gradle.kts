2d6d3538948a311f637a8d11a1f4d451852346c4
Mansoor ul haq <mansoorulhaq166@gmail.com>
2025-03-17 11:22:00 +0500

    UI Updated

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.guessmaster.challenge"
         minSdk = 23
         targetSdk = 35
-        versionCode = 3
-        versionName = "1.2.0"
+        versionCode = 4
+        versionName = "1.3.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
